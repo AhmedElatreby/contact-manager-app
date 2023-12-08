761290df5e1d837c62611aed3819c9637badcd56
ahmed El-Atreby <ahmedsport@hotmail.com>
2023-12-08 13:35:01 +0000

    JWT middleware validation

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,11 +5,13 @@ const {
   loginUser,
   currentUser,
 } = require("../controllers/userController");
+const validateToken = require("../middleware/validateTokenHandler");
 
 router.post("/register", registerUser);
 
 router.post("/login", loginUser);
 
-router.get("/current", currentUser);
+router.get("/current", validateToken, currentUser);
+
 
 module.exports = router;
